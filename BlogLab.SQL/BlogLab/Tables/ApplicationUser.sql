f9a672bf321734795ddfb545f12e27db02b55646
David Reyes Pucheta <davidpucheta@gmail.com>
2023-02-02 14:23:11 -0600

    Add DB project and ApplicationUser and Photo tables

---
+++ BlogLab.SQL/BlogLab/Tables/ApplicationUser.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[ApplicationUser]
+(
+	[ApplicationUserId] INT NOT NULL IDENTITY (1,1) PRIMARY KEY,
+	[Username] VARCHAR(20) NOT NULL,
+	[NormalizedUsername] VARCHAR(20) NOT NULL,
+	[Email] VARCHAR(30) NOT NULL,
+	[NormalizedEmail] VARCHAR(30) NOT NULL,
+	[Fullname] VARCHAR(30) NULL,
+	[PasswordHash] VARCHAR(MAX) NOT NULL, 
+
+	INDEX [IX_ApplicationUser_NormalizedUsername] ([NormalizedUsername]),
+	INDEX [IX_ApplicationUser_NormalizedEmail] ([NormalizedEmail])
+);
+
---
+++ BlogLab.SQL/BlogLab/Tables/Photo.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Photo]
+(
+	[PhotoId] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [ApplicationUserId] INT NOT NULL, 
+    [PublicId] VARCHAR(50) NOT NULL, 
+    [ImageUrl] VARCHAR(250) NOT NULL, 
+    [Description] VARCHAR(30) NOT NULL, 
+    [PublishDate] DATETIME NOT NULL DEFAULT GETDATE(), 
+    [UpdateDate] DATETIME NOT NULL DEFAULT GETDATE(), 
+
+    CONSTRAINT [FK_Photo_ApplicationUser] FOREIGN KEY ([ApplicationUserId]) REFERENCES [ApplicationUser]([ApplicationUserId])
+)
