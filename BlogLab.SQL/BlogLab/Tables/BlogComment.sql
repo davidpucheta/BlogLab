37f25121e5c03afba56626f547c75eecd9af0edc
David Reyes Pucheta <davidpucheta@gmail.com>
2023-02-02 14:46:04 -0600

    Add BlogComment table
    add new table for blog comments

---
+++ BlogLab.SQL/BlogLab/Tables/BlogComment.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[BlogComment]
+(
+	[BlogCommentId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ParentBlogCommentId] INT NOT NULL, 
+    [BlogId] INT NOT NULL, 
+    [ApplicationUserId] INT NOT NULL, 
+    [Content] VARCHAR(300) NOT NULL, 
+    [PublishDate] DATETIME NOT NULL DEFAULT GETDATE(), 
+    [UpdateDate] DATETIME NOT NULL DEFAULT GETDATE(), 
+    [ActiveInd] BIT NOT NULL DEFAULT CONVERT(BIT, 1), 
+    CONSTRAINT [FK_BlogComment_Blog] FOREIGN KEY ([BlogId]) REFERENCES [Blog]([BlogId]), 
+    CONSTRAINT [FK_BlogComment_ApplicationUser] FOREIGN KEY ([ApplicationUserId]) REFERENCES [ApplicationUser]([ApplicationUserId])
+)
