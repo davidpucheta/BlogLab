09ca9b65bba2ae2e3cf7df7f4be2a5d58bbc6f59
David Reyes Pucheta <davidpucheta@gmail.com>
2023-02-03 17:29:40 -0600

    Create Blog Delete sproc

---
+++ BlogLab.SQL/BlogLab/Stored Procedures/Blog_Get.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[Blog_Get]
+	@BlogId INT
+AS
+    SELECT
+        [BlogId]
+        ,[ApplicationUserId]
+        ,[Username]
+        ,[Title]
+        ,[Content]
+        ,[PhotoId]
+        ,[PublishDate]
+        ,[UpdateDate]
+        ,[ActiveInd]
+    FROM 
+        [aggregate].[Blog] B 
+    WHERE
+       B.BlogId = @BlogId 
+    AND
+        B.ActiveInd = CONVERT(BIT, 1);
+RETURN 0
