c6e4abbd7682cf1e8e3b03f45ca8bd1bdcd0818c
David Reyes Pucheta <davidpucheta@gmail.com>
2023-02-02 19:55:37 -0600

    Add new view
    New view for Blog Comments

--- BlogLab.SQL/Aggregate/Views/Blog.sql
+++ BlogLab.SQL/Aggregate/Views/Blog.sql
@@ -1,18 +1,14 @@
-﻿CREATE VIEW [aggregate].[Blog]
-	AS 
-		SELECT
-			B.BlogId,
-			B.ApplicationUserId,
-			AU.Username,
-			B.Title,
-			B.Content,
-			B.PhotoId,
-			B.PublishDate,
-			B.UpdateDate,
-			B.ActiveInd
-		FROM 
-			[dbo].[Blog] B
-		INNER JOIN 
-			dbo.ApplicationUser AU
-		ON
-			B.ApplicationUserId = AU.ApplicationUserId
+﻿CREATE VIEW [aggregate].[Blog] AS
+SELECT
+    B.BlogId,
+    B.ApplicationUserId,
+    AU.Username,
+    B.Title,
+    B.Content,
+    B.PhotoId,
+    B.PublishDate,
+    B.UpdateDate,
+    B.ActiveInd
+FROM
+    [dbo].[Blog] B
+    INNER JOIN dbo.ApplicationUser AU ON B.ApplicationUserId = AU.ApplicationUserId
---
+++ BlogLab.SQL/Aggregate/Views/BlogComment.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW [aggregate].[BlogComment] AS
+SELECT
+    BC.BlogCommentId,
+    BC.ParentBlogCommentId,
+    BC.BlogId,
+    BC.Content,
+    AU.Username,
+    BC.ApplicationUserId,
+    BC.PublishDate,
+    BC.UpdateDate,
+    BC.ActiveInd
+FROM
+    [dbo].[BlogComment] BC
+    INNER JOIN dbo.ApplicationUser AU ON BC.ApplicationUserId = AU.ApplicationUserId;
