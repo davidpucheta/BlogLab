f099c7360816ff3f426e08300cdb4f47f921d4c2
David Reyes Pucheta <davidpucheta@gmail.com>
2023-02-03 17:12:21 -0600

    Add account insert sproc

---
+++ BlogLab.SQL/BlogLab/Stored Procedures/Account_Insert.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [dbo].[Account_Insert] @Account AccountType READONLY AS
+INSERT INTO
+    [dbo].[ApplicationUser] (
+        [Username],
+        [NormalizedUsername],
+        [Email],
+        [NormalizedEmail],
+        [Fullname],
+        [PasswordHash]
+    )
+SELECT
+    [Username],
+    [NormalizedUsername],
+    [Email],
+    [NormalizedEmail],
+    [Fullname],
+    [PasswordHash]
+FROM
+    @Account;
+
+SELECT
+    CAST(SCOPE_IDENTITY() AS INT);
+
+RETURN 0
