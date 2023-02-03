6ada29629829f27979c4feedc7211176770ad832
David Reyes Pucheta <davidpucheta@gmail.com>
2023-02-03 17:13:13 -0600

    Format SQL

--- BlogLab.SQL/BlogLab/Stored Procedures/Account_Insert.sql
+++ BlogLab.SQL/BlogLab/Stored Procedures/Account_Insert.sql
@@ -1,24 +1,26 @@
-﻿CREATE PROCEDURE [dbo].[Account_Insert] @Account AccountType READONLY AS
-INSERT INTO
-    [dbo].[ApplicationUser] (
+﻿CREATE PROCEDURE [dbo].[Account_Insert] 
+    @Account AccountType READONLY 
+AS
+    INSERT INTO
+        [dbo].[ApplicationUser] (
+            [Username],
+            [NormalizedUsername],
+            [Email],
+            [NormalizedEmail],
+            [Fullname],
+            [PasswordHash]
+        )
+    SELECT
         [Username],
         [NormalizedUsername],
         [Email],
         [NormalizedEmail],
         [Fullname],
         [PasswordHash]
-    )
-SELECT
-    [Username],
-    [NormalizedUsername],
-    [Email],
-    [NormalizedEmail],
-    [Fullname],
-    [PasswordHash]
-FROM
-    @Account;
+    FROM
+        @Account;
 
-SELECT
-    CAST(SCOPE_IDENTITY() AS INT);
+    SELECT
+        CAST(SCOPE_IDENTITY() AS INT);
 
 RETURN 0
