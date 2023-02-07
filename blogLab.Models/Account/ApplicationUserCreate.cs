6d7e5c06e88bdef127b0e564363a4300bf7f082b
David Reyes Pucheta <davidpucheta@gmail.com>
2023-02-07 12:38:08 -0600

    Create account models

---
+++ blogLab.Models/Account/ApplicationUserCreate.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogLab.Models.Account;
+
+public class ApplicationUserCreate : ApplicationUserLogin
+{
+    [Required(ErrorMessage = "Fullname is required.")]
+    [MinLength(10, ErrorMessage = "Must be at least 10-30 characters.")]
+    [MaxLength(30, ErrorMessage = "Must be at least 10-30 characters.")]
+    public string Fullname { get; set; }
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
+    [MaxLength(30, ErrorMessage = "Can be at most 30 characters.")]
+    public string Email { get; set; }
+}
