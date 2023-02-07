d0723a3f8a629ae79225bcc4c3fd9e2282a41fd0
David Reyes Pucheta <davidpucheta@gmail.com>
2023-02-07 10:50:44 -0600

    Add delete blog comment sproc

---
+++ BlogLab.SQL/BlogLab/Stored Procedures/BlogComment_Delete.sql
@@ -0,0 +1,46 @@
+﻿CREATE PROCEDURE [dbo].[BlogComment_Delete]
+	@BlogCommentId INT
+AS
+	
+	DROP TABLE IF EXISTS #BlogCommentsToBeDeleted;
+
+	WITH cte_Blog_Comments As (
+		SELECT 
+			BC1.[BlogCommentId],
+			BC1.[ParentBlogCommentId]
+		FROM
+			[dbo].[BlogComment] BC1
+		WHERE
+			BC1.[BlogCommentId] = @BlogCommentId
+		UNION ALL
+		SELECT 
+			BC2.[BlogCommentId],
+			BC2.[ParentBlogCommentId]
+		FROM
+			[dbo].[BlogComment] BC2
+		INNER JOIN 
+			cte_Blog_Comments BC3
+		ON 
+			BC3.[BlogCommentId] = BC2.[BlogCommentId]
+	)
+
+	SELECT
+		[BlogCommentId],
+		[ParentBlogCommentId]
+	INTO
+		#BlogCommentsToBeDeleted
+	FROM
+		cte_Blog_Comments;
+
+	UPDATE BC
+	SET 
+		BC.[ActiveInd] = CONVERT(BIT, 0),
+		BC.[UpdateDate] = GETDATE()
+	FROM
+		[dbo].[BlogComment] BC 
+	INNER JOIN
+		#BlogCommentsToBeDeleted BCD
+	ON
+		BC.[BlogCommentId] = BCD.[BlogCommentId]
+	
+RETURN 0
