a3ce4a55f26755961dc1b9013df696a0a69f51a2
David Reyes Pucheta <davidpucheta@gmail.com>
2023-02-07 11:50:58 -0600

    Creating the Photo_Get sproc

---
+++ BlogLab.SQL/BlogLab/Stored Procedures/Photo_Get.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[Photo_Get]
+	@PhotoId INT
+AS
+	SELECT 
+		 P.[PhotoId]
+		,P.[ApplicationUserId]
+		,P.[PublicId]
+		,P.[ImageUrl]
+		,P.[Description]
+		,P.[PublishDate]
+		,P.[UpdateDate]
+	FROM 
+		[dbo].[Photo] P
+	WHERE
+		P.[PhotoId] = @PhotoId
+RETURN 0
