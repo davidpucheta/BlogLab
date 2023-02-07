5ac436b4bcbada687b4df42592849b5da7bb16ed
David Reyes Pucheta <davidpucheta@gmail.com>
2023-02-07 12:01:54 -0600

    Creating the Photo_Insert sproc

---
+++ BlogLab.SQL/BlogLab/Stored Procedures/Photo_Insert.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[Photo_Insert]
+	@Photo PhotoType READONLY,
+	@ApplicationUserId INT
+AS
+	INSERT INTO [dbo].[Photo]
+	(
+			 [ApplicationUserId]
+			,[PublicId]
+			,[ImageUrl]
+			,[Description]
+	)
+	SELECT
+		@ApplicationUserId
+		,[PublicId]
+		,[ImageUrl]
+		,[Description]
+	FROM
+		@Photo;
+
+	SELECt CAST(SCOPE_IDENTITY() AS INT);
+	
+RETURN 0
