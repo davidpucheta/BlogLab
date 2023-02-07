72e8786fbf3be86622c8c4f92dc3cbfb604b7b08
David Reyes Pucheta <davidpucheta@gmail.com>
2023-02-07 11:37:56 -0600

    Creating the BlogComment_Get sproc

---
+++ BlogLab.SQL/BlogLab/Stored Procedures/BlogComment_GetAll.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[BlogComment_GetAll]
+	@BlogId INT
+AS
+	SELECT 
+		 BC.[BlogCommentId]
+		,BC.[ParentBlogCommentId]
+		,BC.[BlogId]
+		,BC.[ApplicationUserId]
+		,BC.[Username]
+		,BC.[Content]
+		,BC.[PublishDate]
+		,BC.[UpdateDate]
+	FROM
+		[aggregate].[BlogComment] BC
+		WHERE
+		BC.[BlogId] = @BlogId
+	AND
+		BC.[ActiveInd] = CONVERT(BIT, 1)
+	ORDER BY
+		BC.[UpdateDate] DESC
+
+RETURN 0
