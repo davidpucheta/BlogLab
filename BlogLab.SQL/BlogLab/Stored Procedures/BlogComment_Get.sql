5b737a7192f27fb24b9240f2ddc2a7553ad06f77
David Reyes Pucheta <davidpucheta@gmail.com>
2023-02-07 11:16:13 -0600

    Add get blog comments sproc

---
+++ BlogLab.SQL/BlogLab/Stored Procedures/BlogComment_Get.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[BlogComment_Get]
+	@BlogCommentId INT
+AS
+	SELECT 
+		 BC.[BlogCommentId]
+		,BC.[ParentBlogCommentId]
+		,BC.[BlogId]
+		,BC.[ApplicationUserId]
+		,BC.[Username]
+		,BC.[Content]
+		,BC.[PublishDate]
+		,BC.[UpdateDate]
+	FROM
+		[aggregate].[BlogComment] BC
+	WHERE
+		BC.[BlogCommentId] = @BlogCommentId
+	AND
+		BC.[ActiveInd] = CONVERT(BIT, 1)
+
+RETURN 0
