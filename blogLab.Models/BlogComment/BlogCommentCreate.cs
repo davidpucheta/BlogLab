24caa7fe1ad8b320a52e411a07ef9753ac392656
David Reyes Pucheta <davidpucheta@gmail.com>
2023-02-07 13:07:56 -0600

    Create blog comment models

---
+++ blogLab.Models/BlogComment/BlogCommentCreate.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogLab.Models.BlogComment;
+
+public class BlogCommentCreate
+{
+    public int BlogCommentId { get; set; }
+
+    public int? ParentBlogCommentId { get; set; }
+
+    public int? BlogId { get; set; }
+
+    [Required(ErrorMessage = "Username is required.")]
+    [MinLength(10, ErrorMessage = "Must be 10-300 characters.")]
+    [MaxLength(300, ErrorMessage = "Must be 10-300 characters.")]
+    public string Content { get; set; }
+}
