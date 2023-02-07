d3cdde9ce86a19bca14a9a20027ee2f531e3c4aa
David Reyes Pucheta <davidpucheta@gmail.com>
2023-02-07 11:54:56 -0600

    Creating the Photo_GetByUserId sproc

---
+++ BlogLab.SQL/BlogLab/Stored Procedures/Photo_GetByUserId.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[Photo_GetByUserId]
+	@ApplicationUserId INT
+AS
+	SELECT 
+		 P.[PhotoId]
+		,P.[ApplicationUserId]
+		,P.[PublicId]
+		,P.[ImageUrl]
+		,P.[Description]
+		,P.[PublishDate]
+		,P.[UpdateDate]
+	FROM
+		[dbo].[Photo] P
+	WHERE
+		P.[ApplicationUserId] = @ApplicationUserId
+
+RETURN 0
