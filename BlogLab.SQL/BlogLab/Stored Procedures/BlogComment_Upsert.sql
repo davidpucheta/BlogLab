4cd5f48832bdc3a7bbeb87d57adf2f7f71869402
David Reyes Pucheta <davidpucheta@gmail.com>
2023-02-07 11:38:08 -0600

    Creating the BlogComment_Upsert sproc

---
+++ BlogLab.SQL/BlogLab/Stored Procedures/BlogComment_Upsert.sql
@@ -0,0 +1,41 @@
+﻿CREATE PROCEDURE [dbo].[BlogComment_Upsert]
+	@BlogComment BlogCommentType READONLY,
+	@ApplicationUserId INT
+AS
+	MERGE INTO [dbo].[BlogComment] TARGET
+	USING (
+		SELECT 
+			[BlogCommentId],
+			[ParentBlogCommentId],
+			[BlogId],
+			[Content],
+			@ApplicationUserId [ApplicationUserId]
+		FROM
+			@BlogComment
+	) AS SOURCE
+	ON (
+		TARGET.[BlogCommentId] = SOURCE.[BlogCommentId]
+		AND
+		TARGET.[ApplicationUserId] = SOURCE.[ApplicationUserId]
+	)
+	WHEN MATCHED THEN
+		UPDATE SET
+			TARGET.[Content] = SOURCE.[Content],
+			TARGET.[UpdateDate] = GETDATE()
+	WHEN NOT MATCHED BY TARGET
+		INSERT (
+			[ParentBlogCommentId],
+			[BlogId],
+			[ApplicationUserId],
+			[Content]
+		)
+		VALUES (
+			SOURCE.[ParentBlogCommentId],
+			SOURCE.[BlogId],
+			SOURCE.[ApplicationUserId],
+			SOURCE.[Content],
+		);
+
+	SELECT CAST(SCOPE_IDENTITY() AS INT);
+
+RETURN 0
