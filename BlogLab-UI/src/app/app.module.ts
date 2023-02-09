6dff329a4f7668db2f4a0c2a0a3b37caf7aa975a
David Reyes Pucheta <davidpucheta@gmail.com>
2023-02-09 17:26:19 -0600

    Generate needed components

--- BlogLab-UI/src/app/app.module.ts
+++ BlogLab-UI/src/app/app.module.ts
@@ -5,11 +5,39 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SummaryPipe } from './pipes/summary.pipe';
+import { BlogComponent } from './components/blog-component/blog/blog.component';
+import { BlogCardComponent } from './components/blog-component/blog-card/blog-card.component';
+import { BlogsComponent } from './components/blog-component/blogs/blogs.component';
+import { FamousBlogsComponent } from './components/blog-component/famous-blogs/famous-blogs.component';
+import { CommentBoxComponent } from './components/comment-components/comment-box/comment-box.component';
+import { CommentSystemComponent } from './components/comment-components/comment-system/comment-system.component';
+import { CommentsComponent } from './components/comment-components/comments/comments.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { PhotoAlbumComponent } from './components/photo-album/photo-album.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SummaryPipe
+    SummaryPipe,
+    BlogComponent,
+    BlogCardComponent,
+    BlogsComponent,
+    FamousBlogsComponent,
+    CommentBoxComponent,
+    CommentSystemComponent,
+    CommentsComponent,
+    DashboardComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    PhotoAlbumComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
