5a51c45b3c404b7ec5e967ac6c4b759d3858231b
David Reyes Pucheta <davidpucheta@gmail.com>
2023-02-13 14:58:33 -0600

    Buil registration form

--- BlogLab-UI/src/app/components/register/register.component.ts
+++ BlogLab-UI/src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApplicationUserCreate } from 'src/app/models/account/application-user-create.model';
+import { AccountService } from 'src/app/services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +11,87 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  registerForm: FormGroup;
+
+  constructor(
+    private accountService: AccountService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ){
+    this.registerForm = this.formBuilder.group({
+      fullname: [
+        null,
+        [
+          Validators.minLength(10),
+          Validators.maxLength(30)
+        ]
+      ],
+      username: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(20)
+        ]
+      ],
+      email: [
+        null,
+        [
+          Validators.required,
+          Validators.pattern(/^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/),
+          Validators.maxLength(30)
+        ]
+      ],
+      password: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(40)
+        ]
+      ],
+      confirmPassword: [
+        null,
+        [
+          Validators.required
+        ]
+     ]
+    });
+  }
+
+  formHasError(error: string) {
+    return !!this.registerForm.hasError(error);
+  }
+
+  isTouched(field: string) {
+    return this.registerForm.get(field)?.touched;
+  }
+
+  hasErrors(field: string) {
+    return this.registerForm.get(field)?.errors;
+  }
+
+  hasError(field: string, error: string) {
+    return !!this.registerForm.get(field)?.hasError(error);
+  }
+
+ /* matchValue: ValidatorFn = (fg: FormGroup ) => {
+    const password = fg.get('password')?.value;
+    const confirmPassword = fg.get('confirmPassword')?.value;
+
+    return password === confirmPassword ? { isMatching: false } : { isMatching: true };
+  }*/
+
+  onSubmit() {
+    let applicationUserCreate: ApplicationUserCreate = new ApplicationUserCreate(
+      this.registerForm.get('username')?.value,
+      this.registerForm.get('password')?.value,
+      this.registerForm.get('email')?.value,
+      this.registerForm.get('fullname')?.value,
+    );
+
+    this.accountService.register(applicationUserCreate).subscribe(() => {
+      this.router.navigate(['/dashboard']);
+    });
+  }
 }
