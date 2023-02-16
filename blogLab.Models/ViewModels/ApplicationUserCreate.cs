c4bbf5f0297fd736ac2bc17ad7fbf47aae9409ff
David Reyes Pucheta <davidpucheta@gmail.com>
2023-02-16 13:37:23 -0600

    Add blazor omponent

---
+++ blogLab.Models/ViewModels/ApplicationUserCreate.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogLab.Models.ViewModels;
+
+public class ApplicationUserCreate
+{
+    [Required]
+    public string Username { get; set; }
+
+    [Required]
+    [MinLength(5, ErrorMessage = "Min length 5")]
+    [MaxLength(30, ErrorMessage = "Max 30")]
+    public string Password { get; set; }
+    
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Required]
+    public string Fullname { get; set; }
+}
